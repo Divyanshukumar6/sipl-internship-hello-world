d52fc7801b7da29436ccb815811bdd7f4b05e6ab
DIVYANSHU KUMAR <120015500+Divyanshukumar6@users.noreply.github.com>
2023-10-10 09:52:35 +0530

    Add files via upload

---
+++ src/main/java/com/sipl/Divyanshuhello/DivyanshuHelloApplication.java
@@ -0,0 +1,13 @@
+package com.sipl.Divyanshuhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivyanshuHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivyanshuHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sipl/Divyanshuhello/DivyanshuHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipl.Divyanshuhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivyanshuHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
