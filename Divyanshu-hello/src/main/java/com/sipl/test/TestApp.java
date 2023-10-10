15d14ed8f1f7788a9e59483518365508edf5255b
Divyanshukumar6 <dkchy51@gmail.com>
2023-10-10 09:34:13 +0530

    Initial Commit

---
+++ Divyanshu-hello/src/main/java/com/sipl/test/TestApp.java
@@ -0,0 +1,28 @@
+package com.sipl.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+@ComponentScan(basePackages= {"com.sipl.controller"})
+public class TestApp extends SpringBootServletInitializer {
+	public static void main(String args) {
+		SpringApplication.run(TestApp.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestApp.class);
+	}
+	@RequestMapping("/")
+	String home() {
+		return "home";
+	}
+		
+
+}
